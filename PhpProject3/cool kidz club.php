e8e2477c0adeb6c5b51c5c5609f3655a300264f7
Kridec <cedrikpedersen@gmail.com>
2018-04-24 11:38:07 +0200

    man bliver nu redirectet

---
+++ PhpProject3/cool kidz club.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>
+            You logged in good
+        </p>
+        <img src="https://www.ajcity.net/calendar/Cool%20Kidz%20Club%20copy.jpg" alt="Cool kidz club" height="42" width="42"> 
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>

4651d0cd0f138dd61796417242a9c519386fa6d6
Kridec <cedrikpedersen@gmail.com>
2018-05-08 15:16:21 +0200

    man bliver nu redirectet

--- PhpProject3/cool kidz club.php
+++ PhpProject3/cool kidz club.php
@@ -7,15 +7,27 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Cool Kidz Club</title>
+        <style>
+            img {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 40%;
+            }
+            p {
+                text-align: center
+                    
+            }
+        </style>
     </head>
     <body>
+        <img src="https://www.ajcity.net/calendar/Cool%20Kidz%20Club%20copy.jpg" alt="Cool kidz club">
         <p>
             You logged in good
         </p>
-        <img src="https://www.ajcity.net/calendar/Cool%20Kidz%20Club%20copy.jpg" alt="Cool kidz club" height="42" width="42"> 
-        <?php
-        // put your code here
-        ?>
+        <form action="Door.php" method="post">
+            <input type="submit" name="logout" value="Log ud">
+        </form>
     </body>
 </html>

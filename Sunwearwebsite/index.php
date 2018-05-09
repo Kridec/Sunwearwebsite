50ebd9a968e6c6c5b2e2ec882495e94766d352f7
Kridec <cedrikpedersen@gmail.com>
2018-05-09 11:38:27 +0200

    asger did shit

---
+++ Sunwearwebsite/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+body {margin:0;
+ background-image: url("strand.jpg");
+ background-repeat: no-repeat;
+ background-size: cover;
+
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  opacity: 1;
+  width: 70%;
+  margin: 0 auto;
+  margin-top: 2%;
+}
+
+.navbar p {
+  display: block;
+  color: skyblue;
+  text-align: center;
+  font-size: 70px;
+  line-height: 0px;
+  letter-spacing: 20px;
+  font-weight: bold;
+}
+
+#left {
+      overflow: hidden;
+  background-color: skyblue;
+  opacity: 1;
+  margin-left: 15%;
+  width: 14%;
+  margin-top: 2%;
+  height:700px;
+  float:left;
+}
+
+#right {
+      overflow: hidden;
+  background-color: blue ;
+  opacity: 1;
+  width: 54%;
+  margin-top: 2%;
+  margin-right: 15%;
+  height:700px;
+  margin-left:2px;
+  float:right;
+}
+</style>
+</head>
+<body>
+
+<div class="navbar">
+  <p>SUNWEAR</p>
+
+    </div>
+
+    <div ID="left">
+          
+        
+        
+        
+    </div>
+
+    
+    <div ID="right">
+          
+        
+        
+        
+    </div>
+        
+        
+        
+        
+        
+        
+    </body>
+</html>
